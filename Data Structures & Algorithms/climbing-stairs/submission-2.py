0c5b45d9ad43fa8bddae78ff1fde1bd59791663f
Elliot Daschner <156715976+Elliot-Daschner@users.noreply.github.com>
2026-07-16 17:28:28 -0500

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n in [0, 1, 2]:
+            return n
+        
+        prev1, prev2 = 2, 1
+        result = 0
+
+        for _ in range(n - 2):
+            prev1, prev2 = prev1 + prev2, prev1
+        
+        return prev1      
