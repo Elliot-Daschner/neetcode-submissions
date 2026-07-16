33f687c4821d845d4bbcfd8c84998e0e2ef70082
Elliot Daschner <156715976+Elliot-Daschner@users.noreply.github.com>
2026-07-16 18:36:49 -0500

    Update: missing-number - submission-0

--- Data Structures & Algorithms/missing-number/submission-0.py
+++ Data Structures & Algorithms/missing-number/submission-0.py
@@ -1,5 +1,4 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        n = len(nums)
-        exp = n * (n + 1) // 2
-        return exp - sum(nums)
+        
+       return ((len(nums) * (len(nums)+1)) // 2 - sum(nums))
