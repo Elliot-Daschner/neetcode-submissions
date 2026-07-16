332a500f72ac37b8b04bda89e41a902c98399776
Elliot Daschner <156715976+Elliot-Daschner@users.noreply.github.com>
2026-07-16 13:05:47 -0500

    Update: invert-a-binary-tree - submission-0

--- Data Structures & Algorithms/invert-a-binary-tree/submission-0.py
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-0.py
@@ -8,12 +8,10 @@
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
-            return
-        # swap left and right children
-        l = root.left
-        root.left = root.right
-        root.right = l
-        # traverse down tree
+            return None
+        
+        root.left, root.right = root.right, root.left
+
         self.invertTree(root.left)
         self.invertTree(root.right)
 
