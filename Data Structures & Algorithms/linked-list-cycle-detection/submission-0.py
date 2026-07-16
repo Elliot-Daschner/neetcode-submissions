dec72e216c5c3655dbb176d416ebcdcc5b71f05b
Elliot Daschner <156715976+Elliot-Daschner@users.noreply.github.com>
2026-07-16 10:11:09 -0500

    Update: linked-list-cycle-detection - submission-0

--- Data Structures & Algorithms/linked-list-cycle-detection/submission-0.py
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-0.py
@@ -6,12 +6,12 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        visited = set()
-        while head:
-            if head in visited:
-                return True
-            visited.add(head)
-            head = head.next
-
-        return False
+        seen = set()
         
+        while head:
+            if head.next in seen:
+                return True
+            seen.add(head)
+            head = head.next
+        
+        return False
